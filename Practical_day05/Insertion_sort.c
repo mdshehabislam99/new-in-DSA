be36651f0161def8cb1aaf9329f2af73f11834ed
shehabislam99 <shihabkhanahab@gmail.com>
2026-09-18 22:34:09 +0530

    insertion sort

---
+++ Practical_day05/Insertion_sort.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int key;
+    for (int i = 1; i < n; i++) {
+        key = arr[i];
+       int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+    int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    insertionSort(a, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
